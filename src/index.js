7f0bf5754aee2584c17ac93a9ebc668a5a27ed5d
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-06-04 18:29:28 -0400

    Lesson 4

--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-//const greeting = React.createElement('h1', {}, 'Hello World!');
-const getCurrentDate = () => {
-    const date = new Date();
-    return date.toDateString();
-}
-const greeting = <h1>Hello there Current date: {getCurrentDate()}</h1>
 
-ReactDOM.render(greeting, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
