755d1bc4f1a30f79e35e18657335e88222d70a59
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-06-04 20:53:47 -0400

    Finished Lessons

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import Series from '../../containers/series'
-import Intro from '../Intro';
+import Main from '../Main';
 import './App.css';
 import 'whatwg-fetch';
 
@@ -21,8 +20,7 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">TV Series List</h1>
         </header>
-        <Intro message="Here you can find all of your most loved series"/>
-        <Series />
+        <Main />
       </div>
     );
   }
--- src/components/SeriesList/index.js
+++ src/components/SeriesList/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './index.css';
+import { Link } from 'react-router-dom';
 
-const SeriesListItem = ({series}) => (
-    <li key={series.show.id}>
-        {series.show.name}
+const SeriesListItem = ({ series }) => (
+    <li>
+        <Link to={`/series/${series.show.id}`}>
+            {series.show.name}
+        </Link>
+        
     </li>
 )
 
